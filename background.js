0e063bb4b384c321c64b18c0c8ffc1ecdf42bfa1
Anne Nygaard <annch@itu.dk>
2025-02-25 10:54:58 +0100

    background ekstra commit

--- background.js
+++ background.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-let clickedWord = "";
-
-// Create the context menu
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "extractWord",
-    title: "Extract clicked word",
-    contexts: ["all"]
-  });
-});
-
-// Listen for messages from content script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "setClickedWord") {
-    clickedWord = message.word;
-    console.log("Word received in background script:", clickedWord);
-    sendResponse({ status: "word received" });
-  }
-});
-
-// Log the clicked word when the context menu is clicked
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  console.log("Context menu clicked, word is:", clickedWord);
-});
-=======
 let clickedParagraph = "";
 
 // Create the context menu
@@ -46,4 +20,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     sendResponse({ status: "No paragraph found" });
   }
 });
->>>>>>> something
