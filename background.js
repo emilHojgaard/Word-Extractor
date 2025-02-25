b5595ffc2112d8eb8c6a5f4399bc06517cf0cbc8
Anne Nygaard <annch@itu.dk>
2025-02-25 10:26:42 +0100

    UI popup empty object

--- background.js
+++ background.js
@@ -12,7 +12,11 @@ chrome.runtime.onInstalled.addListener(() => {
 // Listen for messages from content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.paragraph) {
-    console.log("Paragraph received in background script:", message.paragraph);
+    console.log("Paragraph received in background script:", message);
     sendResponse({ status: "word received" });
+
+    // Send the paragraph text back to the same tab to display it
+    chrome.tabs.sendMessage(sender.tab.id, { paragraph: message.paragraph });
+    console.log("Message send back from Service worker:", message.paragraph);
   }
 });
