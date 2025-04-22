df5c919bdd082ba6decf7c71f3c28f99361d5da9
emilHojgaard <emilhn94@gmail.com>
2025-04-22 15:57:42 +0200

    blaaa

---
+++ word-extractor-extension/src/TextContainer/TextContainer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import YellowButton from "../YellowButton/YellowButton.jsx";
+
+const TextContainer = ({ text, onClick }) => {
+  return (
+    <div
+      style={{
+        flex: "1",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        background: "white",
+        borderRadius: "8px",
+        boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+        width: "50vw",
+        maxHeight: "50vh",
+        overflow: "hidden",
+        padding: "20px",
+        paddingBottom: "5px",
+      }}
+    >
+      <div
+        style={{
+          flex: "1",
+          overflowY: "auto", // Enables vertical scrolling if content overflows
+          textAlign: "justify",
+          margin: "0 20px",
+          lineHeight: "1.6",
+          fontSize: "18px",
+        }}
+      >
+        {text}
+      </div>
+
+      <div
+        style={{
+          marginTop: "10px",
+          marginBottom: "0px",
+        }}
+      >
+        <YellowButton text="Close" onClick={onClick} />
+      </div>
+    </div>
+  );
+};
+
+export default TextContainer;
--- word-extractor-extension/src/TextSelectionUI.jsx
+++ word-extractor-extension/src/TextSelectionUI.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import YellowButton from "./YellowButton/YellowButton.jsx";
 import SideMenu from "./SideMenu/SideMenu.jsx";
+import TextContainer from "./TextContainer/TextContainer.jsx";
 
 const TextSelectionUI = ({ selectionObject, onClick }) => {
   const overlayStyle = {
     position: "fixed",
     top: "0",
     left: "0",
-    width: "100%",
-    height: "100%",
+    width: "100vw",
+    height: "100vh",
     background: "rgba(0, 0, 0, 0.5)",
     display: "flex",
     justifyContent: "center",
@@ -16,34 +16,10 @@ const TextSelectionUI = ({ selectionObject, onClick }) => {
     zIndex: "1000",
   };
 
-  const popupStyle = {
-    background: "white",
-    padding: "20px",
-    borderRadius: "8px",
-    boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
-    display: "flex",
-    flexDirection: "row",
-    gap: "20px",
-    width: "60%",
-    maxWidth: "800px",
-    textAlign: "left",
-  };
-
-  const contentStyle = {
-    flex: "1",
-    display: "flex",
-    flexDirection: "column",
-  };
-
   return (
     <div id="textOverlay" style={overlayStyle}>
-      <div style={popupStyle}>
-        <div style={contentStyle}>
-          <p style={{ color: "black" }}>{selectionObject.selectedText}</p>
-          <YellowButton text="Close" onClick={onClick} />
-        </div>
-        <SideMenu />
-      </div>
+      <TextContainer text={selectionObject.selectedText} onClick={onClick} />
+      <SideMenu />
     </div>
   );
 };
