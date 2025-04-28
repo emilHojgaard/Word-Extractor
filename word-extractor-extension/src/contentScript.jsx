e0836fa154fa5914e72cd48e95ce37f97b2da7f5
emilHojgaard <emilhn94@gmail.com>
2025-04-28 20:12:16 +0200

    The new from Zeeguu

--- word-extractor-extension/src/contentScript.jsx
+++ word-extractor-extension/src/contentScript.jsx
@@ -1,88 +1,116 @@
-import React, { useEffect } from "react";
-import { createRoot } from "react-dom/client";
+import React, { StrictMode, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import SmallOverlay from "./SmallOverlay.jsx";
 import TextSelectionUI from "./TextSelectionUI.jsx";
-
-const selectionObject = {
-  selection: null,
-  selectedText: "",
-  url: "",
-  documentTitle: "",
-  isOverlayActive: false,
-  x: 0,
-  y: 0,
-};
 
 // container for the React component
 const container = document.createElement("div");
 container.id = "react-overlay-container";
 document.body.appendChild(container);
 
-const root = createRoot(container);
-
 const App = () => {
-  useEffect(() => {
-    //debugging
-    console.log("useEffect called");
+  // Using useState (overlay) to manage overlays
+  const [overlay, setOverlay] = useState(null);
+  const [isOverlayActive, setIsOverlayActive] = useState(false);
 
-    document.addEventListener("mouseup", (event) => {
-      if (
-        event.target.id === "smallOverlay" ||
-        selectionObject.isOverlayActive
-      ) {
-        return;
-      }
+  // Collecting all the info of the selection in one state
+  const [selection, setSelection] = useState({
+    selectedText: "",
+    x: 0,
+    y: 0,
+    url: null,
+    documentTitle: "",
+  });
 
-      //setting the selection object properties
-      selectionObject.x = event.clientX;
-      selectionObject.y = event.clientY;
-      selectionObject.selection = window.getSelection();
-      selectionObject.selectedText = selectionObject.selection.toString();
-      selectionObject.url = window.location.href;
-      selectionObject.documentTitle = document.title;
-
-      // showing small overlay, if selected text
-      if (selectionObject.selectedText.length > 0) {
-        selectionObject.isOverlayActive = true;
-
-        root.render(
-          <SmallOverlay
-            selectionObject={selectionObject}
-            onClick={handleClick}
-            onTimeout={() => {
-              //cleaning up the small overlay
-              root.render(<></>);
-              selectionObject.isOverlayActive = false;
-              window.getSelection().removeAllRanges();
-            }}
-          />
-        );
-      }
-    });
-
-    return () => {
-      document.removeEventListener("mouseup", (event) => {});
-    };
-  }, []);
-
-  let handleClick = (event) => {
-    //cleaning up the small overlay
+  // remove small overlay when clicked and send selected text to background script
+  const handleSmallOverlayClick = (event) => {
     event.stopPropagation();
-    root.render(<></>);
-    selectionObject.isOverlayActive = false;
+    setOverlay(null);
+    setIsOverlayActive(false);
     window.getSelection().removeAllRanges();
 
-    root.render(
+    // Send selected text, URL and documentTitle to the background script
+    chrome.runtime.sendMessage({
+      type: "SELECTED_TEXT",
+      selectedText: selection.selectedText,
+      url: selection.url,
+      documentTitle: selection.documentTitle,
+    });
+
+    setOverlay(
       <TextSelectionUI
-        selectionObject={selectionObject}
-        onClick={() => {
-          root.render(<></>);
-          selectionObject.isOverlayActive = false;
-        }}
+        selectedText={selection.selectedText}
+        url={selection.url}
+        documentTitle={selection.documentTitle}
+        onClick={handleTextSelectionClick}
       />
     );
   };
+
+  const handleTextSelectionClick = () => {
+    setOverlay(null);
+    setIsOverlayActive(false);
+  };
+
+  // removing small overlay when timeout
+  const handleTimeout = () => {
+    console.log("Overlay timed out");
+    setOverlay(null);
+    setIsOverlayActive(false);
+    window.getSelection().removeAllRanges();
+  };
+
+  // useEffect that sets up the eventlistner for mouseup events (runs once on on-mount of the App component)
+  useEffect(() => {
+    // Function for mouseup event listner
+    const handleMouseUp = (event) => {
+      if (event.target.id === "smallOverlay" || isOverlayActive) return;
+
+      const text = window.getSelection().toString();
+      // showing small overlay, if selected text
+      if (text.length > 0) {
+        // Setting state values based on the selection
+        setSelection({
+          selectedText: text,
+          x: event.clientX,
+          y: event.clientY,
+          url: window.location.href,
+          documentTitle: document.title,
+        });
+        setIsOverlayActive(true);
+      }
+    };
+
+    // Adding event listner
+    document.addEventListener("mouseup", handleMouseUp);
+
+    // Cleaning event listener on unmount
+    return () => {
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
+
+  // rendering the overlay when the states are updated.
+  useEffect(() => {
+    if (!isOverlayActive || selection.selectedText.length === 0) return;
+
+    setOverlay(
+      <SmallOverlay
+        onClick={handleSmallOverlayClick}
+        onTimeout={handleTimeout}
+        x={selection.x}
+        y={selection.y}
+      />
+    );
+  }, [selection, isOverlayActive]);
+
+  return <>{overlay}</>;
 };
 
-// Render the App component (allowing use of React)
-root.render(<App />);
+// Initialising the App component by rendering it inside the container (allowing use of React)
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  container
+);
